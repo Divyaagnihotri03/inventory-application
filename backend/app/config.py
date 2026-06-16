635d349574cb468bd15badfb0ffa0e0c54524ed5
Divya Agnihotri <121118964+Divyaagnihotri03@users.noreply.github.com>
2026-06-16 17:45:38 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = ""
+    DATABASE_URL: str = "postgresql://inventory_db_qeuc_user:KIbkMkE0dr2P8i4tNR3hPHdpmIANEZP8@dpg-d8ojbm77f7vs73eqs950-a.ohio-postgres.render.com/inventory_db_qeuc"
     FRONTEND_URL: str = "http://localhost:3000"
 
     class Config:
